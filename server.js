a49d0bd4d59d88c5ed102491c6e22da0f9cbe3f2
User <mirantrix80@gmail.com>
2019-04-01 15:36:00 -0700

    [Chore]: Add ENV Port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
-  res.json({id:"IYGD434HUIh", owner:"Heroku"});
+  res.json({id:"IYGD434HUIh", owner:"Eroku"});
 });
 
 app.listen(port, () => console.log(`${process.env.CONSOLE}`));
