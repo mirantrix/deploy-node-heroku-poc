529742949308f6d5f7c9d8d508aad889f6f67d72
User <mirantrix80@gmail.com>
2019-04-01 15:07:33 -0700

    [Chore]: Add ENV Port

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const env = require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
   res.send('Landing Page');
 });
 
-app.listen(port, () => console.log(`at Port: ${port}`));
+app.listen(port, () => console.log(`at Port: ${port} ${process.env.CONSOLE}`));
