841c81e4ab563d0afa8abecdd8e86e3381d400d7
User <mirantrix80@gmail.com>
2019-04-01 15:40:35 -0700

    [Chore]: Delete dotenv from sevrer.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
-  res.json({id:"IYGD434HUIh", owner:"Eroku"});
+  res.json({id:"IYGD434HUIh", owner:"Heroku"});
 });
 
-app.listen(port, () => console.log(`${process.env.CONSOLE}`));
+app.listen(port);
